49a9bf3bb7e858fca267c52201173158f6292df6
David NoHorizon <d.chartolani@yahoo.com>
2023-05-25 21:36:37 +0400

    digital less1

---
+++ Lesson1/less1.py
@@ -0,0 +1,22 @@
+# Comment
+
+"""
+This is block comment
+This is block comment
+This is block comment
+"""
+
+# Variable
+text = 'Hello World!'
+print(text)
+
+# Concatenation
+fName = "David"
+lName = "Chartolani"
+
+print (fName + " " + lName)
+
+#Snake Case
+
+first_name = "John"
+last_name = "Doe"
