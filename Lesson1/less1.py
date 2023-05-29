ccada69e43b0f55292bef51ef665465bd020fa73
David NoHorizon <d.chartolani@yahoo.com>
2023-05-29 19:25:49 +0400

    digital less1

--- Lesson1/less1.py
+++ Lesson1/less1.py
@@ -14,9 +14,28 @@ print(text)
 fName = "David"
 lName = "Chartolani"
 
-print (fName + " " + lName)
+print(fName + " " + lName)
 
-#Snake Case
+# Snake Case
 
 first_name = "John"
 last_name = "Doe"
+
+
+##############
+print('Hello Luka')
+
+# this is comment
+# varaible definition
+# assignment operator =
+# 'Hello world' <- value / data
+# text = 'Hello World'
+greting = 'hello'  # str data type
+name = 'John'
+
+# snake case
+fist_name = "Givi"
+last_name = "Doe"
+# concatenation
+print(greting + ' ' + name)  # hello John
+print(greting.capitalize() + ' ' + fist_name.upper() + ', ' + last_name.lower())
