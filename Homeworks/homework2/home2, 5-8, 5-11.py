58c5ffaf959a273896f3c633d9cc74eb605e347a
David NoHorizon <d.chartolani@yahoo.com>
2023-06-05 19:39:18 +0400

    home2.py fixed done

--- Homeworks/homework2/home2, 5-8, 5-11.py
+++ Homeworks/homework2/home2, 5-8, 5-11.py
@@ -14,7 +14,7 @@ users = []  # Empty list of users
 
 if users:
     for user in users:
-        if user == 'admin':
+        if user.lower() == 'admin':
             print("Hello admin, would you like to see a status report?")
         else:
             print(f"Hello {user}, thank you for logging in again.")
