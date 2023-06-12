e27e829fbe8866b711ea5e510de3ef978563b7a6
David NoHorizon <d.chartolani@yahoo.com>
2023-06-12 19:51:18 +0400

    home5, 1-2

--- Homeworks/homework4/6-1--6-3.py
+++ Homeworks/homework4/6-1--6-3.py
@@ -1,22 +1,19 @@
-"""
-f_name
-l_name
-age
-city
-"""
-
 print(5 * "#", "6-1", 5 * "#")
+print("")
 
 friend_1 = {
-    'f_name' : "Rati",
-    'l_name' : "Kashibadze",
-    'age' : 29,
-    'city' : "Tbilisi"
+    'f_name': "Rati",
+    'l_name': "Kashibadze",
+    'age': 29,
+    'city': "Tbilisi"
 }
 
-print(friend_1["f_name"], friend_1["l_name"], friend_1["age"], friend_1["city"])
+print(friend_1["f_name"], friend_1["l_name"],
+      friend_1["age"], friend_1["city"])
 
+print("")
 print(5 * "#", "6-2", 5 * "#")
+print("")
 
 fav_nums = {
     'Gio': 10,
@@ -29,15 +26,16 @@ fav_nums = {
 for key, value in fav_nums.items():
     print(f"{key}'s favorite number is: {value}'")
 
-
+print("")
 print(5 * "#", "6-3", 5 * "#")
+print("")
 
 words = {
-    'key' : 'means keys',
-    'values' : 'means values',
-    'ram' : 'temporary memory',
-    'cpu' : "computers's brain",
-    'store' : 'save'
+    'key': 'means keys',
+    'values': 'means values',
+    'ram': 'temporary memory',
+    'cpu': "computers's brain",
+    'store': 'save'
 }
 
 for key, value in words.items():
---
+++ Homeworks/homework4/6-7--6-12.py
@@ -0,0 +1,56 @@
+print(5 * "#", "6-7", 5 * "#")
+print("")
+
+friend_1 = {
+    'f_name': "Rati",
+    'l_name': "Kashibadze",
+    'age': 29,
+    'city': "Tbilisi"
+}
+
+friend_2 = {
+    'f_name': "Lasha",
+    'l_name': "Tskhadadze",
+    'age': 35,
+    'city': "Tbilisi"
+}
+
+friend_3 = {
+    'f_name': "Levani",
+    'l_name': "Gochashvili",
+    'age': 33,
+    'city': "Tbilisi"
+}
+
+
+friends = [friend_1, friend_2, friend_3]
+
+for friend in friends:
+    print("First Name:", friend['f_name'])
+    print("Last Name:", friend['l_name'])
+    print("Age:", friend['age'])
+    print("City:", friend['city'])
+    print()
+
+
+print("")
+print(5 * "#", "6-8", 5 * "#")
+print("")
+
+
+
+print("")
+print(5 * "#", "6-9", 5 * "#")
+print("")
+
+print("")
+print(5 * "#", "6-10", 5 * "#")
+print("")
+
+print("")
+print(5 * "#", "6-11", 5 * "#")
+print("")
+
+print("")
+print(5 * "#", "6-12", 5 * "#")
+print("")
