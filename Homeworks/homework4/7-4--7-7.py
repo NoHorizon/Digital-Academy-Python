c422f71106c2b43cce511c9b53449341ceb7c06b
David NoHorizon <d.chartolani@yahoo.com>
2023-06-15 01:26:47 +0400

    7-4--7-7.py need to finish

---
+++ Homeworks/homework4/7-4--7-7.py
@@ -0,0 +1,43 @@
+# print("")
+# print(5 * "#", "7-4", 5 * "#")
+# print("")
+
+# while True:
+#     topping = input("Enter a pizza topping (or write 'quit' to exit): ")
+#     if topping.lower() == "quit":
+#         break
+#     print("Adding", topping, "to your pizza.")
+
+# print("Your pizza is ready!")
+
+
+# print("")
+# print(5 * "#", "7-5", 5 * "#")
+# print("")
+
+
+# while True:
+#     age = int(input("Enter your age: "))
+
+
+#     if age < 0:
+#         break
+
+#     if age < 3:
+#         ticket_price = 0
+#     elif age >= 3 and age <= 12:
+#         ticket_price = 10
+#     else:
+#         ticket_price = 12
+
+#     print(f"Your movie ticket price is ${ticket_price}")
+
+
+print("")
+print(5 * "#", "7-6", 5 * "#")
+print("")
+
+
+# print("")
+# print(5 * "#", "7-7", 5 * "#")
+# print("")
